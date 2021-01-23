d833d6cba635b789d053a2310ad7776425d8cdea
Jordao Barroso <jordao@andfrankly.com>
2021-01-23 22:49:28 +0100

    login in

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import TwitchIcon from "./TwitchIcon.svg";
 
-const auth = () => {
-  window.open("http://localhost:3000/auth/twitch");
+// const auth = () => {
+//   window.open("http://localhost:3000/auth/twitch");
+// };
+const fetchAuth = () => {
+  fetch("http://localhost:3000/auth/twitch").then(res => console.log(res))
 };
 
 const Login = () => {
   return (
     <div>
-      <button onClick={auth}>
+      <a href="http://localhost:3000/auth/twitch">
+      {/* <button onClick={fetchAuth}> */}
+      {/* <button onClick={auth}> */}
         <img src={TwitchIcon} width="24" height="24" alt="Twitch" />
         Twitch auth
-      </button>
+
+      {/* </button> */}
+      </a>
     </div>
   );
 };
